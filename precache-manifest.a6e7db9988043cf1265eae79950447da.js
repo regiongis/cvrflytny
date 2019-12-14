2c106ba350b3d0ff680e958eba13196e39ce1dfd
Magloire Sendegeya <magloire1@gmail.com>
2019-12-14 19:55:10 +0100

    Updates

--- precache-manifest.a6e7db9988043cf1265eae79950447da.js
+++ precache-manifest.a6e7db9988043cf1265eae79950447da.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ce4669e3315ee7fdeeca2b8d37d3dbc9",
+    "revision": "f57ed87a1a270999a0aa369ee423f342",
     "url": "/cvrflytny/index.html"
   },
   {
-    "revision": "9b83d7267ae3041fe90a",
+    "revision": "d4722f494aa2ed7ae5b1",
     "url": "/cvrflytny/static/css/main.b100e6da.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cvrflytny/static/js/2.94211a8c.chunk.js"
   },
   {
-    "revision": "9b83d7267ae3041fe90a",
-    "url": "/cvrflytny/static/js/main.ab76e044.chunk.js"
+    "revision": "d4722f494aa2ed7ae5b1",
+    "url": "/cvrflytny/static/js/main.6c3fe602.chunk.js"
   },
   {
     "revision": "e392c3fb2d54232e000a",
