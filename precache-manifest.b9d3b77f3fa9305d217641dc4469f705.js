aad5107545e8bf3268717fa0449bdb98a0666cf2
Magloire Sendegeya <902877@edc.dk>
2020-03-09 12:40:49 +0100

    Updates

---
+++ precache-manifest.b9d3b77f3fa9305d217641dc4469f705.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fb13b6c57a8365d4c7746c3c62f905a",
+    "url": "/cvrflyt/index.html"
+  },
+  {
+    "revision": "d71c2b088fdac2989c74",
+    "url": "/cvrflyt/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "55f5e5460ac1ee8fb222",
+    "url": "/cvrflyt/static/js/2.d312e97d.chunk.js"
+  },
+  {
+    "revision": "5547a05bbd3989a0e25df35e1409ccfc",
+    "url": "/cvrflyt/static/js/2.d312e97d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d71c2b088fdac2989c74",
+    "url": "/cvrflyt/static/js/main.1688be10.chunk.js"
+  },
+  {
+    "revision": "dd14469318dcdd4e5677",
+    "url": "/cvrflyt/static/js/runtime-main.bf9448f9.js"
+  }
+]);
