7c314130dce69c6fbe420f3652d959968c72b014
Magloire Sendegeya <902877@edc.dk>
2020-03-09 13:06:55 +0100

    Updates

---
+++ precache-manifest.6227484fad161d19ac8b48af4f2ac781.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff566cd721ee253017b9aecbbb0537e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "505e6bd35e6c7b6941da",
+    "url": "/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "55f5e5460ac1ee8fb222",
+    "url": "/static/js/2.d312e97d.chunk.js"
+  },
+  {
+    "revision": "5547a05bbd3989a0e25df35e1409ccfc",
+    "url": "/static/js/2.d312e97d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "505e6bd35e6c7b6941da",
+    "url": "/static/js/main.8c1a8f27.chunk.js"
+  },
+  {
+    "revision": "f84766ac8faab67fac0c",
+    "url": "/static/js/runtime-main.6ff90ad2.js"
+  }
+]);
